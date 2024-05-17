dde7e32a90c59481ec0bfaf179b8ecff81f11916
luscabap <lucasbaptistasilva.dev@gmail.com>
2024-05-17 20:07:03 -0300

    Test Fetchin test api

--- front-end/pages/teste.tsx
+++ front-end/pages/teste.tsx
@@ -1,3 +1,25 @@
-export default function TestePage(){
-  return <h1>Olá mundo</h1>;
+type teste = {
+  resposta: {
+  }
+}
+
+export const getServerSideProps = (async () => {
+  const requisicao = await fetch("http://localhost:4000/");
+  const resposta = await requisicao.json();
+
+  return {
+    props: {
+      resposta,
+    }
+  };
+});
+
+export default function TestePage({ resposta }: teste){
+  console.log(resposta);
+
+  return (
+    <div>
+      <h1>Olá mundo</h1>
+    </div>
+  );
 }
