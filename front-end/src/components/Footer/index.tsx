cb05abf71539e98130cdfb7062051f44c99e6682
luscabap <lucasbaptistasilva.dev@gmail.com>
2024-05-19 16:22:18 -0300

    :sparkles: Feat: Component Footer

---
+++ front-end/src/components/Footer/index.tsx
@@ -0,0 +1,11 @@
+export default function Footer() {
+  return (
+    <footer className="bg-slate-900 text-center p-2">
+      <p className="text-white">
+        Site desenvolvidor por{" "}
+        <abbr title="lucasbaptistasilva.dev@gmail.com" className="no-underline font-bold text-slate-400 cursor-pointer">Lucas Baptista</abbr> |
+        &copy; 2024 Todos os direitos reservados
+      </p>
+    </footer>
+  );
+}
