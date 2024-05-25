657b76a317412155063d8bb9ef3af73f104d2d85
luscabap <lucasbaptistasilva.dev@gmail.com>
2024-05-25 12:54:45 -0300

    :sparkles: Feat: Making database connection

--- back-end/app.js
+++ back-end/app.js
@@ -1,5 +1,14 @@
 import express from "express";
-import routes from "./routes/index.js";
+import routes from "./src/routes/index.js";
+import db from "./src/config/dbConnect.js";
+
+db.on("error", (erro) => {
+  console.log.bind(console, `Erro de conexão - ${erro}`);
+});
+
+db.once("open", () => {
+  console.error("Conexão com o banco feita com sucesso!");
+})
 
 const app = express();
 app.use(express.json());
---
+++ back-end/src/config/dbConnect.js
@@ -0,0 +1,8 @@
+import mongoose from "mongoose";
+
+mongoose.connect(process.env.DB_CONNECTION_STRING);
+
+
+let db = mongoose.connection;
+
+export default db;
