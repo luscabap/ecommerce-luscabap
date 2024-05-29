46947c11beb744e76775993d98ab2e72adad9c67
luscabap <lucasbaptistasilva.dev@gmail.com>
2024-05-29 12:28:01 -0300

    :sparkles: Feat: new Model - 'doce'

---
+++ back-end/src/models/Doce.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const doceSchema = new mongoose.Schema({
+  id: { type: mongoose.Schema.Types.ObjectId },
+  titulo: { type: String },
+  preco: { type: Number },
+  slug: { type: String },
+  foto: { type: String },
+  categoria: { type: String }
+}, { versionKey: false });
+
+const doce = mongoose.model("doces", doceSchema);
+
+export default doce;
