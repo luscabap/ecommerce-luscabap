37d0859f5543dec029cf2e466e7246b19cc72341
luscabap <lucasbaptistasilva.dev@gmail.com>
2024-06-02 17:11:45 -0300

    :boom: Fix: adding required in schema

--- back-end/src/models/Doce.js
+++ back-end/src/models/Doce.js
@@ -2,13 +2,13 @@ import mongoose from "mongoose";
 
 const doceSchema = new mongoose.Schema({
   id: { type: mongoose.Schema.Types.ObjectId },
-  titulo: { type: String },
-  preco: { type: Number },
-  slug: { type: String },
-  foto: { type: String },
-  categoria: { type: String },
-  peso: { type: Number },
-  serve: { type: Number }
+  titulo: { type: String, required: [true] },
+  preco: { type: Number, required: [true] },
+  slug: { type: String, required: [true] },
+  foto: { type: String, required: [true] },
+  categoria: { type: String, required: [true] },
+  peso: { type: Number, required: [true] },
+  serve: { type: Number, required: [true] }
 }, { versionKey: false });
 
 const doce = mongoose.model("doces", doceSchema);
